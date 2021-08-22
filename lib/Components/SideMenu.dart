ed7e3913a9a96b1abaca46a4637b87b882b4ca4c
Abdelrahman Ali <abdelrahman.ali9191@gmail.com>
2021-08-22 12:14:05 +0200

    Sidemenu implemented with two options

--- lib/Components/SideMenu.dart
+++ lib/Components/SideMenu.dart
@@ -1,9 +1,82 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class  SideMenu extends StatelessWidget {
+class SideMenu extends StatefulWidget {
+  const SideMenu({Key? key}) : super(key: key);
+  static const routeName = 'side menu';
+
+  @override
+  _SideMenuState createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  var colorLightTheme = Colors.white;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Drawer(
+      child: Container(
+        color: colorLightTheme,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 90,horizontal: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                InkWell(
+                  onTap: (){
+
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Text('Collections'),
+                  ),
+                ),
+                InkWell(
+                  onTap: (){
+                    changeLanguage();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Text('Settings'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  void changeLanguage() {
+    Navigator.pop(context);
+    showModalBottomSheet(context: context, builder: (buildContext){
+      return Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            InkWell(
+              onTap: (){
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 12),
+                child: Text('English',textAlign: TextAlign.center,),
+              ),
+            ),
+            InkWell(
+              onTap: (){
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 12),
+                child: Text('Arabic',textAlign: TextAlign.center),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
 }
--- lib/Home.dart
+++ lib/Home.dart
@@ -1,9 +1,47 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:news_application/Components/SideMenu.dart';
+import 'Components/SideMenu.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  static const routeName = 'home';
+  @override
+  _HomeState createState() => _HomeState();
+}
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+class _HomeState extends State<Home> {
+  PageController _myPage = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: scaffoldKey,
+      drawer:SideMenu(),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text('Sports', style: TextStyle(
+          color:Colors.white,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 57, 165, 82),
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(80),
+            bottomRight: Radius.circular(80),
+          )
+        ),
+      ),
+      body:PageView(
+        controller: _myPage,
+        onPageChanged: (int){},
+        children: [
+        ],
+      ),
+    );
   }
 }
