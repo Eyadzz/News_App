0908b89ef4444f723cae061528099c8f207a684b
Abdelrahman Ali <abdelrahman.ali9191@gmail.com>
2021-08-22 19:16:50 +0200

    Modifications for Textfield

--- lib/Home.dart
+++ lib/Home.dart
@@ -7,18 +7,20 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
+
 final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
 class _HomeState extends State<Home> {
   PageController _myPage = PageController(initialPage: 0);
   Icon customIcon = Icon(Icons.search);
   Widget customSearchBar = Text("Sports");
+  SideMenu customized = SideMenu();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-      drawer:SideMenu(),
+      drawer: customized,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         actions: <Widget>[
@@ -26,20 +28,36 @@ class _HomeState extends State<Home> {
             icon: customIcon,
             onPressed: () {
               setState(() {
-                if ( this.customIcon.icon == Icons.search){
+                if (this.customIcon.icon == Icons.search) {
                   this.customIcon = new Icon(Icons.close);
                   this.customSearchBar = new TextField(
                     style: new TextStyle(
-                      color: Colors.white,
-
+                      height: 0.1,
+                      color: Colors.black,
                     ),
                     decoration: new InputDecoration(
-                        prefixIcon: new Icon(Icons.search,color: Colors.white),
-                        hintText: "Search...",
-                        hintStyle: new TextStyle(color: Colors.white)
+                      prefixIcon: new Icon(Icons.close, color: Colors.grey),
+                      suffixIcon: new Icon(Icons.search, color: Colors.grey),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: "Search Article",
+                      hintStyle: new TextStyle(color: Colors.grey,fontWeight: FontWeight.w100,),
+                      contentPadding: const EdgeInsets.only(
+                          left: 14.0, bottom: 4.0, top: 30.0),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: new BorderRadius.circular(25.7),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: new BorderSide(color: Colors.white),
+                        borderRadius: new BorderRadius.circular(25.7),
+                      ),
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                      ),
                     ),
-                  );}
-                else {
+                  );
+                } else {
                   this.customIcon = new Icon(Icons.search);
                   this.customSearchBar = new Text("Sports");
                 }
@@ -53,17 +71,15 @@ class _HomeState extends State<Home> {
         elevation: 0,
         backgroundColor: Color.fromARGB(255, 57, 165, 82),
         shape: ContinuousRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(80),
-            bottomRight: Radius.circular(80),
-          )
-        ),
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(80),
+          bottomRight: Radius.circular(80),
+        )),
       ),
-      body:PageView(
+      body: PageView(
         controller: _myPage,
-        onPageChanged: (int){},
-        children: [
-        ],
+        onPageChanged: (int) {},
+        children: [],
       ),
     );
   }
